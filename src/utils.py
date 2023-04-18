a032a6c492da612671a98e8e8a353dd9acaa34a0
rkdey95@gmail.com <rkdey95@gmail.com>
2023-04-18 22:37:50 +0800

    updated hyperparameter tuning

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import numpy  as np
 import pandas as pd
 import dill
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 from src.logger import logging
@@ -16,19 +17,26 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params):
     try: 
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-            model.fit(X_train,y_train) # Train the model 
+            para = params[list(models.keys())[i]]
+
+            gs = GridSearchCV(model, para, cv= 3, verbose= True)
+            gs.fit(X_train,y_train)
+            # model.fit(X_train,y_train) # Train the model # Redundant after GSCV
+            model.set_params(**gs.best_params_)
+            model.fit(X_train,y_train)
+
             y_train_pred = model.predict(X_train)
             y_test_pred = model.predict(X_test)
             train_model_score = r2_score(y_train,y_train_pred)
             test_model_score = r2_score(y_test,y_test_pred)
             report[list(models.keys())[i]] = test_model_score
             
-            return report
+        return report
 
     except Exception as e: 
         pass
